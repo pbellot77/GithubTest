763029cd6dab48f777b4ca0fed2645d73557f145
Patrick Bellot <patrickbellot77@gmail.com>
2022-04-14 13:39:57 -0500

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Patrick Bellot on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
